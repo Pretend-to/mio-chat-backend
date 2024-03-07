b9e9ed574048558775b13e79dd7590ae3fb11755
Mio-FCIP <1099834705@qq.com>
2024-03-07 19:42:06 +0800

    update prodia official

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { startServer } from "./lib/server.js";
 import taskScheduler from "./lib/corn.js";
+import { statusCheck } from "./lib/check.js";
 
+await statusCheck();
 startServer();
 
 const scheduler = new taskScheduler();
