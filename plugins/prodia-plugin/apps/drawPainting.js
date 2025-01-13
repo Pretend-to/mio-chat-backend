c310dee9b8af6edef68b236a45aea64f6af304d5
Mio Kurumio <mio@krumio.com>
2025-01-13 12:31:52 +0800

    update: 更新部分预设

--- plugins/prodia-plugin/apps/drawPainting.js
+++ plugins/prodia-plugin/apps/drawPainting.js
@@ -1,6 +1,7 @@
-import { MioFunction,Param } from '../../../lib/functions.js'
-import Drawer from '../../../lib/draw.js'
-
+import { MioFunction, Param } from '../../../lib/functions.js'
+import { pluginInfo } from '../index.js'
+import { createProdia } from 'prodia/v2'
+import { bufferToUrl } from '../../../utils/imgTools.js'
 
 export class drawPainting extends MioFunction {
   constructor() {
@@ -11,24 +12,9 @@ export class drawPainting extends MioFunction {
       params: [
         new Param({
           name: 'prompt',
-          type:'string',
+          type: 'string',
           description: 'the prompt of the painting,the prompt must be in english',
-
           required: true
-        }),
-        new Param({
-          name:'style',
-          type:'string',
-          description: 'the style of the painting',
-          required: false,
-          enumeration:['anime','realistic']
-        }),
-        new Param({
-          name:'orientation',
-          type:'string',
-          description: 'the orientation of the painting',
-          required: false,
-          enumeration:['vertical','horizontal','square']
         }),
       ],
     })
@@ -36,19 +22,32 @@ export class drawPainting extends MioFunction {
   }
 
   async drawPainting(e) {
-    const config = e.params
-    const painter = new Drawer()
+    const prompt = e.params.prompt
+    const url = e.user.origin
 
-    const drawConfig = {
-      model: config.style == 'anime' ? 'animagineXLV3_v30.safetensors [75f2f05b]' : 'devlishphotorealism_sdxl15.safetensors [77cba69f]',
-      width: config.orientation =='vertical'? 1 : 
-        config.orientation =='horizontal'? 2 : 1 ,
-      height: config.orientation =='vertical'? 2 : 
-        config.orientation =='horizontal'? 1 : 1 ,
+    const prodia = createProdia({
+      token: pluginInfo.config.token,
+    })
+
+    const job = await prodia.job({
+      'type': 'inference.flux.pro.txt2img.v1',
+      'config': {
+        'prompt': prompt,
+        'guidance_scale': 3,
+        'steps': 25,
+        'width': 1024,
+        'height': 768
+      }
+    }, {
+      accept: 'image/jpeg'
+    })
+
+    const image = await job.arrayBuffer()
+
+    const result = await bufferToUrl(image, url)
+
+    return {
+      url: result
     }
-
-    const result = await painter.draw('prodia.xl',config.prompt,drawConfig)
-
-    return result
   }
 }
