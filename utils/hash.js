f8be12951e3ec85b6a5a415029c6bb2c47c28dbd
Mio Kurumio <mio@krumio.com>
2025-04-25 01:00:02 +0800

    fix: 同名插件调用异常

---
+++ utils/hash.js
@@ -0,0 +1,7 @@
+import crypto from 'crypto';
+
+export function generateHash(input) {
+    const hash = crypto.createHash('md5');
+    hash.update(input);
+    return hash.digest('hex').slice(0, 6);
+}
